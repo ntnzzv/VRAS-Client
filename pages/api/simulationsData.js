a616a6c96772ab6fc0dbe2ac797cbbf3eadaae38
Natan Z <56848492+ntnzzv@users.noreply.github.com>
2022-06-03 14:47:59 +0300

    api changes

--- pages/api/simulationsData.js
+++ pages/api/simulationsData.js
@@ -1,14 +1,19 @@
-import { collection, addDoc, getDocs } from "firebase/firestore";
-import firebase from "../../firebase/firebase";
+import { collection, addDoc, getDocs, updateDoc, doc, increment, getDoc } from "firebase/firestore";
+import db from "../../firebase/firebase";
 export default async function handler(req, res) {
   if (!req.headers.authorization || req.headers.authorization !== process.env.MY_API) {
     return res.status(401).json({ error: "Incorrect API key" });
   }
-  const databaseRef = collection(firebase, "simulations");
+  const databaseRef = collection(db, "simulations");
+  const countRefDoc = doc(db, "simulationsCount", "lastId");
 
   if (req.method === "POST") {
+    await updateDoc(countRefDoc, { id: increment(1) });
+    const lastid = await getDoc(countRefDoc);
+    req.body.id = lastid;
     addDoc(databaseRef, req.body).catch((err) => {
       console.error(err);
+      updateDoc(countRefDoc, { id: increment(-1) });
       return res.status(500).json({ error: `${err}` });
     });
     return res.status(200).json(req.body);
